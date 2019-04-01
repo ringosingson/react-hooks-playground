0cb891dfc8e5a45796f1bfb5ec519b15a3409346
Ringo Singson <ringosingson@live.com>
2019-04-02 00:16:07 +0530

    useState example 2

--- src/App.js
+++ src/App.js
@@ -8,9 +8,26 @@ const App = () => {
     <div className="main-wrapper">
       <h1>Level Up Dishes</h1>
       <h3>{name}</h3>
-      <input type="text" onChange={e => setName(e.target.value)} value={name} />
+      <form
+        onSubmit={e => {
+          e.preventDefault();
+          formSubmit(name, setName);
+        }}
+      >
+        <input
+          type="text"
+          onChange={e => setName(e.target.value)}
+          value={name}
+        />
+        <button>Submit</button>
+      </form>
     </div>
   );
+};
+
+const formSubmit = (value, setValue) => {
+  console.log("email sent to" + value + "!");
+  setValue("");
 };
 
 export default App;
