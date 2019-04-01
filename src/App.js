ef8d2e1c371f6414eb8d8df2e06755e4e891bde3
Ringo Singson <ringosingson@live.com>
2019-04-02 00:30:15 +0530

    useEffect example

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+//useEffect run after every complete render(after mount and update)
 
 const App = () => {
   // const [value, setValue] = useState(initialState);
   const [name, setName] = useState("");
 
+  useEffect(() => {
+    document.title = name; //documeent is browser api, here the title is change
+  });
+
   return (
     <div className="main-wrapper">
       <h1>Level Up Dishes</h1>
-      <h3>{name}</h3>
+
       <form
         onSubmit={e => {
           e.preventDefault();
-          formSubmit(name, setName);
         }}
       >
         <input
@@ -23,11 +27,6 @@ const App = () => {
       </form>
     </div>
   );
-};
-
-const formSubmit = (value, setValue) => {
-  console.log("email sent to" + value + "!");
-  setValue("");
 };
 
 export default App;
