755b43da234ae87900f02371029d5fae8af1f6ec
Ringo Singson <ringosingson@live.com>
2019-04-03 04:12:58 +0530

    fixing the stuck scrol after toggle

--- src/hooks/bodyScrollLock.js
+++ src/hooks/bodyScrollLock.js
@@ -2,7 +2,13 @@ import { useLayoutEffect } from "react";
 
 function useBodyScrollLock() {
   useLayoutEffect(() => {
+    const originalOverflow = window.getComputedStyle(document.body).overflow;
     document.body.style.overflow = "hidden";
+
+    // return function cleanup()
+    return () => {
+      document.body.style.overflow = originalOverflow;
+    };
   });
 }
 
