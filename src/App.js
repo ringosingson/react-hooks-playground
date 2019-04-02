5027e4bcaa55d2526f58848a5c2a651b54a0e2ef
Ringo Singson <ringosingson@live.com>
2019-04-02 14:47:24 +0530

    useContext

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 //eslint-disable-next-line
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState, useEffect, useRef, createContext } from "react";
+import Toggle from "./Toggle";
 import { useTitleInput } from "./hooks/useTitleInput";
 //useEffect run after every complete render(after mount and update)
+export const UserContext = createContext();
 
 const App = () => {
   // const [value, setValue] = useState(initialState);
@@ -12,25 +14,32 @@ const App = () => {
   // });
 
   return (
-    <div className="main-wrapper" ref={ref}>
-      <h1 onClick={() => ref.current.classList.add("new-fake-class")}>
-        {/*when we click on h1 new class is added to it */}
-        Level Up Dishes
-      </h1>
+    <UserContext.Provider
+      value={{
+        user: true
+      }}
+    >
+      <div className="main-wrapper" ref={ref}>
+        <h1 onClick={() => ref.current.classList.add("new-fake-class")}>
+          {/*when we click on h1 new class is added to it */}
+          Level Up Dishes
+        </h1>
+        <Toggle />
 
-      <form
-        onSubmit={e => {
-          e.preventDefault();
-        }}
-      >
-        <input
-          type="text"
-          onChange={e => setName(e.target.value)}
-          value={name}
-        />
-        <button>Submit</button>
-      </form>
-    </div>
+        <form
+          onSubmit={e => {
+            e.preventDefault();
+          }}
+        >
+          <input
+            type="text"
+            onChange={e => setName(e.target.value)}
+            value={name}
+          />
+          <button>Submit</button>
+        </form>
+      </div>
+    </UserContext.Provider>
   );
 };
 
