d37a76444669d6438c175e8bb92e862de41c0a07
Ringo Singson <ringosingson@live.com>
2019-04-03 03:46:01 +0530

    fetching data with useEffect

--- src/App.js
+++ src/App.js
@@ -12,11 +12,25 @@ const App = () => {
   // useEffect(() => {
   //  document.title = name; //documeent is browser api, here the title is change
   // });
+  const [dishes, setDishes] = useState([]);
+
+  const fetchDishes = async () => {
+    const res = await fetch(
+      "https://my-json-server.typicode.com/leveluptuts/fakeapi/dishes"
+    );
+    const data = await res.json();
+    setDishes(data);
+  };
+
+  useEffect(() => {
+    fetchDishes();
+  }, []);
 
   return (
     <div className="main-wrapper" ref={ref}>
       <h1 onClick={() => ref.current.classList.add("new-fake-class")}>
         {/*when we click on h1 new class is added to it */}
+        Level up Hooks
       </h1>
       <Toggle />
 
@@ -32,6 +46,17 @@ const App = () => {
         />
         <button>Submit</button>
       </form>
+      {dishes.map(dish => (
+        <article key={dish.id} className="dish-card dish-card--withImage">
+          <h3>{dish.name}</h3>
+          <p>{dish.desc}</p>
+          <div className="ingredients">
+            {dish.ingredients.map(ingredient => (
+              <span>{ingredient}</span>
+            ))}
+          </div>
+        </article>
+      ))}
     </div>
   );
 };
