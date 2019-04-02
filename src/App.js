18321600e736a9b27f80385939d4a534da9bd996
Ringo Singson <ringosingson@live.com>
2019-04-02 14:30:46 +0530

    useRef example

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
 //eslint-disable-next-line
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { useTitleInput } from "./hooks/useTitleInput";
 //useEffect run after every complete render(after mount and update)
 
 const App = () => {
   // const [value, setValue] = useState(initialState);
   const [name, setName] = useTitleInput("");
-
+  const ref = useRef();
   // useEffect(() => {
   //  document.title = name; //documeent is browser api, here the title is change
   // });
 
   return (
-    <div className="main-wrapper">
-      <h1>Level Up Dishes</h1>
+    <div className="main-wrapper" ref={ref}>
+      <h1 onClick={() => ref.current.classList.add("new-fake-class")}>
+        {/*when we click on h1 new class is added to it */}
+        Level Up Dishes
+      </h1>
 
       <form
         onSubmit={e => {
